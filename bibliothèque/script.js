7c22489d4f3e68678fdc0ddd54501260a677f0a9
Clément Szewczyk <clement.szewczyk@gmail.com>
2022-05-29 14:52:34 +0200

    MAJ 29 mai 15h
    Ajout Header footer
    Ajout texte epage découverte

--- bibliothèque/script.js
+++ bibliothèque/script.js
@@ -1,8 +1,8 @@
 //TYPED
 
 var typed = new Typed('.typed', {
-    strings: ['Bonjour '],
-    typeSpeed: 20,
+    strings: ["Jeune start-up innovatrice spécialisée dans la vente de grande distribution pour tout type de besoin. Vous pourrez désormais y trouver pratiquement tout ce dont vous souhaitez, du plus commun (maillot de foot...) au plus farfelu (eau du bain, bac...) en passant par les plus petits produits (PQ, ESP32...) au plus imposants (clio 4...). Adibaba compte bien se développer et connaître une expansion dans les 5 années à venir dans le but de devenir un concurrent sérieux avec Alibaba. L'idéal serait d'être numéro grâce à nos articles très diversifiés. Adibaba est la plteforme idéale pour satisfaire n'importe lequel de vos besoins. Bientôt, vous pourrez acheter un mobil-home, mais aussi des diamants ou encore une rolex..."],
+    typeSpeed: 100,
     backSpeed: 0,
     loop: false
   });
